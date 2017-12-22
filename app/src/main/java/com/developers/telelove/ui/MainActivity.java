17462dad1b669a99309eb637fa4a83709c6bcade
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2017-12-23 02:44:33 +0530

    feat: setup dependencies

--- app/src/main/java/com/developers/telelove/ui/MainActivity.java
+++ app/src/main/java/com/developers/telelove/ui/MainActivity.java
@@ -1,14 +1,25 @@
-package com.developers.telelove;
+package com.developers.telelove.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.developers.telelove.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.shows_recycler_view)
+    RecyclerView showsRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
 
     }
 }
